0faa66bfbd7f71b4b8b27052af0c29b80a75b1cf
AryanChakma <jhonuchakma@gmail.com>
2024-12-15 00:28:14 +0530

    Updated

--- src/store/actions/movieActions.jsx
+++ src/store/actions/movieActions.jsx
@@ -1,4 +1,4 @@
-import axios from "../../utils/axios";
+import axios from "../../Utils/axios";
 import { loadmovie } from "../reducers/movieSlice";
 export { removemovie } from "../reducers/movieSlice";
 
--- src/store/actions/tvActions.jsx
+++ src/store/actions/tvActions.jsx
@@ -1,4 +1,4 @@
-import axios from "../../utils/axios";
+import axios from "../../Utils/axios";
 import { loadtv } from "../reducers/tvSlice";
 export { removetv } from "../reducers/tvSlice";
 
